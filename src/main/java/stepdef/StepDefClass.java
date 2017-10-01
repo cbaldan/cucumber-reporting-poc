23a0869cb14ace93b76034460189c39cbc4aa819
Cleber Baldan <cleber.baldan@gmail.com>
2017-10-01 17:42:57 -0300

    Moved cucumber-reporting to verify phase

--- src/main/java/stepdef/StepDefClass.java
+++ src/main/java/stepdef/StepDefClass.java
@@ -1,21 +1,15 @@
 package stepdef;
 
-//import org.apache.log4j.Logger;
-
 import cucumber.api.java.en.Then;
 
 public class StepDefClass {
 
-//	private static final Logger LOGGER = Logger.getLogger(StepDefClass.class);
-
 	@Then("^This step is run$")
 	public void this_step_is_run() throws Throwable {
-//		LOGGER.info("The one step was run");
 	}
-	
+
 	@Then("^This step fails$")
 	public void this_step_fails() throws Throwable {
-//		LOGGER.info("The fail step");
 		throw new RuntimeException();
 	}
 
